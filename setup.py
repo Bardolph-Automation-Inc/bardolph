fa5311248e6fd8e8f7b93c6dffdff23fc4087d4e
al-fontes <alfred@fontes.org>
2019-11-12 23:17:31 -0800

    Bump the revision number.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="bardolph",
-    version="0.0.15",
+    version="0.0.16",
     author="Al Fontes",
     author_email="bardolph@fontes.org",
     description="Simple script interpreter for LIFX light bulbs",
