eb586510ee0769625a732a05b03f52655c916fa4
Perry Piazza <perry@bardolphauto.com>
2025-09-07 16:22:21 -0700

    Windows: use msvcrt in get_key to replace termios; add timeout-safe polling

--- bardolph/controller/get_key.py
+++ bardolph/controller/get_key.py
@@ -1,13 +1,33 @@
+# bardolph/controller/get_key.py
 import sys
-import termios
-import tty
 
-def getch():
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(fd)
-        ch = sys.stdin.read(1)
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return ch
+if sys.platform.startswith("win"):
+    import msvcrt
+
+    def getch(timeout=None):
+        if timeout is None:
+            ch = msvcrt.getch()
+            return ch.decode("utf-8", errors="ignore")
+        # simple timed poll
+        import time
+        start = time.time()
+        while True:
+            if msvcrt.kbhit():
+                ch = msvcrt.getch()
+                return ch.decode("utf-8", errors="ignore")
+            if time.time() - start >= float(timeout):
+                return ""
+else:
+    import termios, tty, select
+
+    def getch(timeout=None):
+        fd = sys.stdin.fileno()
+        old = termios.tcgetattr(fd)
+        try:
+            tty.setraw(fd)
+            if timeout is None:
+                return sys.stdin.read(1)
+            r, _, _ = select.select([sys.stdin], [], [], float(timeout))
+            return sys.stdin.read(1) if r else ""
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old)
